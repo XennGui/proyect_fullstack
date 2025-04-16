a0116747d9bca943a6fce11748be19cf9643414b
XennGui <xen.luna07@gmail.com>
2025-04-16 15:52:28 -0500

    Agregando comentarios necesarios

--- BACKEND/controller/clientController.js
+++ BACKEND/controller/clientController.js
@@ -8,7 +8,7 @@ class ClientController {
             res.json(clients);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al obtener los clientes' });
+            res.status(500).json({ message: 'Error al obtener los clientes' }); //error 500 Error interno del servidor (problema inesperado)
         }
     }
 
@@ -17,12 +17,12 @@ class ClientController {
         try {
             const client = await clientService.getClientByDNI(dni);
             if (!client) {
-                return res.status(404).json({ message: 'Cliente no encontrado' });
+                return res.status(404).json({ message: 'Cliente no encontrado' }); //error 404 Cliente no encontrado
             }
             res.json(client);
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al obtener el cliente' });
+            res.status(500).json({ message: 'Error al obtener el cliente' }); //error 500 Error interno del servidor (problema inesperado)
         }
     }
 
@@ -32,10 +32,10 @@ class ClientController {
             const newClient = await clientService.addClient({
                 dni, nombre, apellido_paterno, apellido_materno, fecha_nacimiento
             });
-            res.status(201).json(newClient);
+            res.status(201).json(newClient);  // error 201 Cliente creado exitosamente
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al crear el cliente' });
+            res.status(500).json({ message: 'Error al crear el cliente' }); //error 500 Error interno del servidor (problema inesperado)
         }
     }
 
@@ -47,11 +47,11 @@ class ClientController {
             if (updatedCliente) {
                 res.json(updatedCliente);
             } else {
-                res.status(404).json({ message: 'Cliente no encontrado' });
+                res.status(404).json({ message: 'Cliente no encontrado' });  //error 404 Cliente no encontrado  
             }
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al actualizar el cliente' });
+            res.status(500).json({ message: 'Error al actualizar el cliente' }); //error 500 Error interno del servidor (problema inesperado)
         }
 
     }
@@ -60,10 +60,10 @@ class ClientController {
         try {
             const { dni } = req.params;
             await clientService.removeClient(dni);
-            res.sendStatus(204);
+            res.sendStatus(204);  //error 204 Cliente eliminado exitosamente
         } catch (error) {
             console.error(error);
-            res.status(500).json({ message: 'Error al eliminar el cliente' });
+            res.status(500).json({ message: 'Error al eliminar el cliente' }); //error 500 Error interno del servidor (problema inesperado)
         }
     }
 }
