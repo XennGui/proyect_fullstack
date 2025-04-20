83d4abd74be6cefb56b6bf8ccaac86daa8167bdd
XennGui <xen.luna07@gmail.com>
2025-04-19 22:11:38 -0500

    Agregando Endpoint para productos

--- BACKEND/routes/productRouters.js
+++ BACKEND/routes/productRouters.js
@@ -1,21 +1,25 @@
-//BACKEND/routes/productRouters.js
+// BACKEND/routes/productRouters.js
+
 const express = require('express');
 const router = express.Router();
 const productController = require('../controller/productController');
 
-//Solicitud de tipo GET en la url
-router.get('/',(req, res) => productController.getProducts(req, res));
+// GET todos los productos
+router.get('/', (req, res) => productController.getProducts(req, res));
 
-//Solicitud de tipo GET en la url
+// Obtener productos por cliente_id
+router.get('/by-client', (req, res) => productController.getProductsByClientId(req, res));
+
+// GET producto por ID
 router.get('/:id',(req, res) => productController.getProductById(req, res));
 
-//Solicitud de tipo POST en la url
-router.post('/',(req, res) => productController.createProduct(req, res));
+// POST crear producto
+router.post('/', (req, res) => productController.createProduct(req, res));
 
-//Solicitud de tipo PUT en la url
-router.put('/:id',(req, res) => productController.updateProduct(req, res));
+// PUT actualizar producto
+router.put('/:id', (req, res) => productController.updateProduct(req, res));
 
-//Solicitud de tipo DELETE en la url
-router.delete('/:id',(req, res) => productController.deleteProduct(req, res));
+// DELETE eliminar producto
+router.delete('/:id', (req, res) => productController.deleteProduct(req, res));
 
 module.exports = router;
